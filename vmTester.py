45bb7b0143e3b02c4d20107d82a3a89ea16b5d34
Taiki <tnsakai@gmail.com>
2022-08-02 15:52:32 -0700

    tesing py on vm

---
+++ vmTester.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 13:17:27 2022
+Testing VM data load
+@author: tnsak
+"""
+import numpy as np
+import os
+
+zcDir = 'data/WigNP_WL128_Int8_Indiv/ZC'
+zcNp = os.listdir(zcDir)
+
+oneArr = np.load(os.path.join(zcDir, zcNp[0]))
+print(oneArr.mean())
