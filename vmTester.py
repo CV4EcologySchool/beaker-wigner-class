9c842615c843cc17f97a4f4105f2011f6d49cafd
Taiki <tnsakai@gmail.com>
2022-08-05 15:34:54 -0700

    testing new git url and updated vmtester

--- vmTester.py
+++ vmTester.py
@@ -7,9 +7,12 @@ Testing VM data load
 import numpy as np
 import os
 
-zcDir = 'WigNP_WL128_Int8_Indiv/ZC'
+# zcDir = '/home/tsakai/CV4Ecology/data/WigNP_WL128_Int8_Indiv/ZC'
+zcDir = 'data/WigNP_WL128_Int8_Indiv/ZC'
 zcNp = os.listdir(zcDir)
 
-oneArr = np.load(os.path.join(zcDir, zcNp[0]))
-print(zcNp[0])
+oneArr = np.load(os.path.join(zcDir, 'PASCAL_28_J_OE16_257007734_C2.npy'))
+# print(zcNp[0])
 print(oneArr.mean())
+#%%
+# [(index, value) for index, value in enumerate(zcNp) if value == 'PASCAL_28_J_OE16_257007734_C2.npy']
